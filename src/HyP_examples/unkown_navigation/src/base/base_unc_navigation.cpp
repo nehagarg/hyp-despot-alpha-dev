2661ac72bd0039d62aab7d4920ae5c928d87e0d5
nehagarg <priyadarshini.neha@gmail.com>
2018-12-07 18:43:40 +0800

    Fixed minimum number of particles in inital belief

--- src/HyP_examples/unkown_navigation/src/base/base_unc_navigation.cpp
+++ src/HyP_examples/unkown_navigation/src/base/base_unc_navigation.cpp
@@ -419,7 +419,11 @@ void BaseUncNavigation::LineRobPos(UncNavigationState* startState, int start_lin
 }
 
 Belief* BaseUncNavigation::InitialBelief(const State* start, string type) const {
-	int N = 10000; //Globals::config.num_scenarios*10;
+	int N = Globals::config.num_scenarios*10;
+	if (N < 10000)
+	{
+		N = 10000;
+	}
 
 	vector<State*> particles(N);
 	if(FIX_SCENARIO==1)
